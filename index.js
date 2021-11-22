1ad67e2d593f497bcd08d25ab7e7a26107c4d19d
NiloufarPadkan <niloufar.pa@gmail.com>
2021-11-22 15:13:37 +0330

    changed token header to authorization header

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Methods",
         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, token");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
--- middleware/verification/loginVerify.js
+++ middleware/verification/loginVerify.js
@@ -5,12 +5,12 @@ const Response = require("../../services/responses/general");
 
 const verifyToken = async (req, res, next) => {
     let response = new Response();
-
-    if (!req.headers.token) {
+    console.log(req.headers["authorization"]);
+    if (!req.headers.authorization) {
         response.setStatus(400).setMessage("fail").setRes(dict.enterToken);
         return res.status(400).send(response.handler());
     }
-    const token = req.headers.token.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     jwt.verify(token, process.env.JWT_KEY, async (err, admin) => {
         if (err) {
