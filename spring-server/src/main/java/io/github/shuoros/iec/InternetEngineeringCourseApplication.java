954e4efdb822e258e11b736f5758f8eb0dcd7663
Soroush <s_shemshadi@ymail.com>
2021-12-02 12:34:55 +0330

    [INITIAL] Spring Boot Application

---
+++ spring-server/src/main/java/io/github/shuoros/iec/InternetEngineeringCourseApplication.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.iec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetEngineeringCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetEngineeringCourseApplication.class, args);
+    }
+
+}
---
+++ spring-server/src/test/java/io/github/shuoros/iec/InternetEngineeringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.iec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetEngineeringCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
