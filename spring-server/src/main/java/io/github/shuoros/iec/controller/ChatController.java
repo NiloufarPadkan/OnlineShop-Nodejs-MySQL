68cb9f25cff7217bb9de82c8281d23221f574979
Soroush <s_shemshadi@ymail.com>
2021-12-11 10:52:39 +0330

    [ADD] missing imports

--- spring-server/src/main/java/io/github/shuoros/iec/controller/ChatController.java
+++ spring-server/src/main/java/io/github/shuoros/iec/controller/ChatController.java
@@ -1,4 +1,31 @@
 package io.github.shuoros.iec.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.lang.invoke.MethodHandles;
+import java.security.Principal;
+
+@Controller
 public class ChatController {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping()
+    public void chat(Message<Object> message, @Payload String payload, Principal principal) throws Exception {
+
+    }
 }
