cea5541d5ca5842cf328c1b741916f18785f8b68
NiloufarPadkan <niloufar.pa@gmail.com>
2021-12-19 11:30:46 +0330

    update admin fixed

--- NodeJS Application/services/rootService/admin.js
+++ NodeJS Application/services/rootService/admin.js
@@ -49,9 +49,15 @@ exports.updateAdmin = async (req) => {
         }
         const role = (await foundAdmin.getRole()).role; /*get role of admin*/
         if (role === "root") return "rootCantBeEdited";
+        const username = req.body.username ? req.body.username : foundAdmin.username;
+
+        const email = req.body.email ? req.body.email : foundAdmin.email;
+
+        const phone = req.body.phone ? req.body.phone : foundAdmin.phone;
+        const roleId = req.body.roleId ? req.body.roleId : foundAdmin.roleId;
         const duplicateUsername = await Admin.findOne({
             where: {
-                username: req.body.username,
+                username: username,
             },
         });
         if (duplicateUsername && duplicateUsername.id !== foundAdmin.id) {
@@ -59,7 +65,7 @@ exports.updateAdmin = async (req) => {
         }
         const duplicateEmail = await Admin.findOne({
             where: {
-                email: req.body.email ? req.body.email : "",
+                email: email,
             },
         });
         if (duplicateEmail && duplicateEmail.id !== foundAdmin.id) {
@@ -67,35 +73,36 @@ exports.updateAdmin = async (req) => {
         }
         const duplicatephone = await Admin.findOne({
             where: {
-                phone: req.body.phone ? req.body.phone : "",
+                phone: phone,
             },
         });
         if (duplicatephone && duplicatephone.id !== foundAdmin.id) {
             return "duplicatePhone";
         }
+        let saltHash;
+        if (req.body.password) saltHash = genPassword(req.body.password);
 
-        const saltHash = genPassword(req.body.password);
-
-        const foundRole = await Role.findByPk(req.body.roleId);
+        const foundRole = await Role.findByPk(roleId);
         if (!foundRole) {
             return "roleNotfound";
         }
 
         const upadmin = await Admin.findByPk(adminId).then((admin) => {
-            admin.roleId = req.body.roleId ? req.body.roleId : admin.roleId;
+            admin.roleId = roleId;
 
             admin.activityStatus = req.body.activityStatus
                 ? req.body.activityStatus
                 : foundAdmin.activityStatus;
 
-            admin.username = req.body.username ? req.body.username : admin.username;
+            admin.username = username;
 
-            admin.email = req.body.email ? req.body.email : admin.email;
+            admin.email = email;
 
-            admin.phone = req.body.phone ? req.body.phone : admin.email;
+            admin.phone = phone;
 
-            admin.hash = saltHash.hash;
-            admin.salt = saltHash.salt;
+            admin.hash = req.body.password ? saltHash.hash : admin.hash;
+
+            admin.salt = req.body.password ? saltHash.salt : admin.salt;
 
             return admin.save();
         });
