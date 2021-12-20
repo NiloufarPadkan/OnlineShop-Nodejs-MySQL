5183f9ccce58f12a3ad4be7a421a7491b27f8d0a
Soroush <s_shemshadi@ymail.com>
2021-12-20 10:10:29 +0330

    [ADD] chat parameters

--- spring-server/src/main/java/io/github/shuoros/iec/model/Chat.java
+++ spring-server/src/main/java/io/github/shuoros/iec/model/Chat.java
@@ -1,4 +1,24 @@
 package io.github.shuoros.iec.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Chat {
+
+    @Id
+    private String id;
+    private String admin;
+    private String user;
+    private String message;
+    private Date date;
+
 }
