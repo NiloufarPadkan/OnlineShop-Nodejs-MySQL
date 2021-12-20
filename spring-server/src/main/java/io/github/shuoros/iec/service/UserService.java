088df79e40a2b3cbb43f7b52941172638602e1aa
Soroush <s_shemshadi@ymail.com>
2021-12-20 10:20:59 +0330

    [ADD] user service methods

--- spring-server/src/main/java/io/github/shuoros/iec/service/UserService.java
+++ spring-server/src/main/java/io/github/shuoros/iec/service/UserService.java
@@ -1,7 +1,36 @@
 package io.github.shuoros.iec.service;
 
+import io.github.shuoros.iec.model.User;
+import io.github.shuoros.iec.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public User save(User user) {
+        return repo.save(user);
+    }
+
+    public Optional<User> get(String id) {
+        return repo.findById(id);
+    }
+
+    public boolean exist(String id) {
+        return repo.existsById(id);
+    }
+
+    public List<User> all() {
+        return repo.findAll();
+    }
+
+    public void delete(String id) {
+        repo.deleteById(id);
+    }
 }
