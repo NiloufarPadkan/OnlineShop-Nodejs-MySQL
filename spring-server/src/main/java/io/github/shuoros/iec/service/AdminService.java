efb7e8e2be5d73c2d1854584bf754730795dcc22
Soroush <s_shemshadi@ymail.com>
2021-12-20 10:22:23 +0330

    [ADD] admin service methods

--- spring-server/src/main/java/io/github/shuoros/iec/service/AdminService.java
+++ spring-server/src/main/java/io/github/shuoros/iec/service/AdminService.java
@@ -1,7 +1,36 @@
 package io.github.shuoros.iec.service;
 
+import io.github.shuoros.iec.model.Admin;
+import io.github.shuoros.iec.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminService {
+
+    @Autowired
+    private AdminRepository repo;
+
+    public Admin save(Admin admin) {
+        return repo.save(admin);
+    }
+
+    public Optional<Admin> get(String id) {
+        return repo.findById(id);
+    }
+
+    public boolean exist(String id) {
+        return repo.existsById(id);
+    }
+
+    public List<Admin> all() {
+        return repo.findAll();
+    }
+
+    public void delete(String id) {
+        repo.deleteById(id);
+    }
 }
