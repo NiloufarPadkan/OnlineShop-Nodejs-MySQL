1d2a41613cce483eda15e356ec43da0bf1c58cc7
Soroush <s_shemshadi@ymail.com>
2021-12-20 10:23:29 +0330

    [ADD] chat service methods

--- spring-server/src/main/java/io/github/shuoros/iec/service/ChatService.java
+++ spring-server/src/main/java/io/github/shuoros/iec/service/ChatService.java
@@ -1,7 +1,38 @@
 package io.github.shuoros.iec.service;
 
+import io.github.shuoros.iec.model.Admin;
+import io.github.shuoros.iec.model.Chat;
+import io.github.shuoros.iec.repository.AdminRepository;
+import io.github.shuoros.iec.repository.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChatService {
+
+    @Autowired
+    private ChatRepository repo;
+
+    public Chat save(Chat chat) {
+        return repo.save(chat);
+    }
+
+    public Optional<Chat> get(String id) {
+        return repo.findById(id);
+    }
+
+    public boolean exist(String id) {
+        return repo.existsById(id);
+    }
+
+    public List<Chat> all() {
+        return repo.findAll();
+    }
+
+    public void delete(String id) {
+        repo.deleteById(id);
+    }
 }
