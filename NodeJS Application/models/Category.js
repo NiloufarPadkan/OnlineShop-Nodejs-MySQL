66810ef6e9c3a634ac12376ddb6acc014a8ddbdb
NiloufarPadkan <niloufar.pa@gmail.com>
2021-12-23 11:41:18 +0330

    subcategories added #4

--- NodeJS Application/models/Category.js
+++ NodeJS Application/models/Category.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../config/database/sequelize");
 
-const Category = sequelize.define("category", {
+const Category = sequelize.define("Category", {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -16,7 +16,25 @@ const Category = sequelize.define("category", {
         unique: true,
     },
     photo: {
-        type: Sequelize.BLOB,
+        type: Sequelize.STRING,
+        get: function () {
+            return JSON.parse(this.getDataValue("photo"));
+        },
+        set: function (val) {
+            return this.setDataValue("photo", JSON.stringify(val));
+        },
+    },
+    parentId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+    },
+    activityStatus: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        set: function (val) {
+            if (val === 1) return this.setDataValue("activityStatus", true);
+            else return this.setDataValue("activityStatus", false);
+        },
     },
 });
 
