21e926f6b4a48868edd966df5ed840ee3cd9e40c
Soroush <s_shemshadi@ymail.com>
2022-01-16 16:55:43 +0330

    [ADD] web client builder bean

--- spring-server/src/main/java/io/github/shuoros/iec/InternetEngineeringCourseApplication.java
+++ spring-server/src/main/java/io/github/shuoros/iec/InternetEngineeringCourseApplication.java
@@ -2,12 +2,19 @@ package io.github.shuoros.iec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class InternetEngineeringCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InternetEngineeringCourseApplication.class, args);
+    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
     }
 
 }
