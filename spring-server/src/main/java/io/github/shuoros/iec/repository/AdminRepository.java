45ff20390bc8976d6a6178e2a9bbc6c27740865b
Soroush <s_shemshadi@ymail.com>
2022-01-18 12:33:20 +0330

    [ADD] get admin by username from database

--- spring-server/src/main/java/io/github/shuoros/iec/repository/AdminRepository.java
+++ spring-server/src/main/java/io/github/shuoros/iec/repository/AdminRepository.java
@@ -1,9 +1,16 @@
 package io.github.shuoros.iec.repository;
 
 import io.github.shuoros.iec.model.Admin;
+import io.github.shuoros.iec.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
+
+    @Query("{'username' : ?0}")
+    List<Admin> findByUsername(int username);
 }
