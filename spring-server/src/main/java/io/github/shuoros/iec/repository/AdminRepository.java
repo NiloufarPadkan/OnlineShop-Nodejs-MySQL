77e9487c89946cf1cf22338d70819e89aaa1435a
Soroush <s_shemshadi@ymail.com>
2022-01-18 12:33:55 +0330

    [UPDATE] optimize imports

--- spring-server/src/main/java/io/github/shuoros/iec/repository/AdminRepository.java
+++ spring-server/src/main/java/io/github/shuoros/iec/repository/AdminRepository.java
@@ -1,7 +1,6 @@
 package io.github.shuoros.iec.repository;
 
 import io.github.shuoros.iec.model.Admin;
-import io.github.shuoros.iec.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
