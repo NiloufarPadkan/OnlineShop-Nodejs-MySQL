ea99ff85b6f3d4c02c5767cb26a05338ce446b46
Soroush <s_shemshadi@ymail.com>
2022-01-18 12:33:09 +0330

    [ADD] get user by username from database

--- spring-server/src/main/java/io/github/shuoros/iec/repository/UserRepository.java
+++ spring-server/src/main/java/io/github/shuoros/iec/repository/UserRepository.java
@@ -2,8 +2,14 @@ package io.github.shuoros.iec.repository;
 
 import io.github.shuoros.iec.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    @Query("{'username' : ?0}")
+    List<User> findByUsername(int username);
 }
