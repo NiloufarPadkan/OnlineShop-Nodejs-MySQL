8e43eba094b0d41de0fb9026fe9e7f7b876b462d
NiloufarPadkan <niloufar.pa@gmail.com>
2022-01-23 14:21:50 +0330

    updateing orders by admin added

--- NodeJS Application/Admin/services/order/order.js
+++ NodeJS Application/Admin/services/order/order.js
@@ -2,7 +2,6 @@ const Order = require("../../../models/Order");
 const Product = require("../../../models/Product");
 const Customer = require("../../../models/Customer");
 const Sequelize = require("sequelize");
-
 const Op = Sequelize.Op;
 
 exports.show = async (req, res, next) => {
@@ -49,4 +48,19 @@ exports.index = async (req, res, next) => {
     } catch (error) {
         throw new Error(e);
     }
+};
+exports.update = async (req, res, next) => {
+    const id = req.params.id;
+
+    let order = await Order.findOne({
+        where: {
+            id: id,
+        },
+    });
+
+    let status = req.body.status ? req.body.status : order.status;
+    let address = req.body.address ? req.body.address : order.address;
+    order.status = status;
+    order.address = address;
+    return order;
 };
