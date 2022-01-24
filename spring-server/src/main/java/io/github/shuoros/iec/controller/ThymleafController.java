4aaa8633454d028d12efc66f1b26a7a1dd0cbb4a
Soroush <s_shemshadi@ymail.com>
2022-01-24 23:13:27 +0330

    [ADD] get unique chats and their users serve in html

--- spring-server/src/main/java/io/github/shuoros/iec/controller/ThymleafController.java
+++ spring-server/src/main/java/io/github/shuoros/iec/controller/ThymleafController.java
@@ -1,7 +1,10 @@
 package io.github.shuoros.iec.controller;
 
+import io.github.shuoros.iec.service.ChatService;
+import io.github.shuoros.iec.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 @Controller
 public class ThymleafController {
 
     private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Autowired
+    public ChatService chatService;
 
     @RequestMapping("/")
     public String index(@RequestParam(defaultValue = "null") String jwt, @RequestParam(defaultValue = "null") String who,
@@ -21,6 +28,10 @@ public class ThymleafController {
         log.info("<=== handleIndexResource: session=" + jwt + ", ip=" + request.getRemoteAddr() + ", user-agent="
                 + request.getHeader("User-Agent"));
         if ("admin".equals(who)) {
+            List<Integer> users = chatService.getUniqueUsers();
+            List<String> chats = chatService.getUniqueChatsLastMessage();
+            model.addAttribute("users", "users");
+            model.addAttribute("chats", "chats");
             model.addAttribute("who", "admin");
         } else if ("user".equals(who)) {
             model.addAttribute("who", "user");
